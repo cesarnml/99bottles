bf8ecf8c189b500efc47f2c9e758a1c3e9a8afab
CesarNML <cesarnml@outlook.com>
2025-04-20 19:32:01 +0700

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    node: true,
+    jest: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended'],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 2015,
+    sourceType: 'module',
+    requireConfigFile: false,
+  },
+  rules: {
+    eqeqeq: ['error', 'always'],
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+      },
+    ],
+    quotes: ['error', 'double'],
+    'no-unused-vars': 'warn',
+    'no-console': 'warn',
+    semi: 'never',
+    'no-undef': 'never',
+  },
+}
