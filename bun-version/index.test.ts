a75eeaa422a155f816c1493f72662b8010fc0b57
CesarNML <cesarnml@outlook.com>
2025-04-29 19:26:53 +0700

    bun version

---
+++ bun-version/index.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, test } from 'bun:test'
+import { Bottles } from './index'
+
+describe('Bottles', () => {
+  test('the first verse', () => {
+    const expected =
+      '99 bottles of beer on the wall, 99 bottles of beer.\n' +
+      'Take one down and pass it around, 98 bottles of beer on the wall.\n'
+    expect(new Bottles().verse(99)).toBe(expected)
+  })
+})
---
+++ bun-version/index.ts
@@ -0,0 +1,10 @@
+export class Bottles {
+  song() {}
+  verses(high: number, low: number) {}
+  verse(number: number) {
+    return (
+      `99 bottles of beer on the wall, 99 bottles of beer.\n` +
+      `Take one down and pass it around, 98 bottles of beer on the wall.\n`
+    )
+  }
+}
